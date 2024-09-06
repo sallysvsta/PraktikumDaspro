5745ca82f5c108ff9669e7668e0bcb161321328f
sallysvsta <sallyysavista@gmail.com>
2024-09-06 09:36:18 +0700

    commit minggu pertama

---
+++ MingguPertama/MyFirstJava19.java
@@ -0,0 +1,8 @@
+public class MyFirstJava19 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Sally Savista Anindya");
+        System.out.print("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+
+}
+}
